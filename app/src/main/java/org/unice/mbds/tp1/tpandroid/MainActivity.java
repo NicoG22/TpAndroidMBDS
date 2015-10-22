2f8a14473d84ba749e42fb0ae17d299359a6b254
Gael Rottier <gaelrottier@gmail.com>
2015-10-22 11:03:20 +0200

    OnClick sur les boutons d enregistrement et de connexion

--- app/src/main/java/org/unice/mbds/tp1/tpandroid/MainActivity.java
+++ app/src/main/java/org/unice/mbds/tp1/tpandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void signin(View view){
+        Intent i = new Intent(this, SigninActivity.class);
+        startActivity(i);
     }
 }
