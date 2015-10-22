f18be4ffb3d03797a55add277dafe78a57ce2647
Nicolas Gauche <gn005920@etu.unice.fr>
2015-10-22 15:42:59 +0200

    - Hint dans register activity - Ecran orientation lock - Centrage texte sign in - Nom par défaut dans le menu

--- app/src/main/java/org/unice/mbds/tp1/tpandroid/MenuActivity.java
+++ app/src/main/java/org/unice/mbds/tp1/tpandroid/MenuActivity.java
@@ -2,12 +2,20 @@ package org.unice.mbds.tp1.tpandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    TextView viewNomPrenom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        viewNomPrenom = (TextView) findViewById(R.id.txt_menu_nom_prenom);
+
+        /** TODO LOAD FROM DATABASE **/
+        viewNomPrenom.setText("Jean Michel");
     }
 }
