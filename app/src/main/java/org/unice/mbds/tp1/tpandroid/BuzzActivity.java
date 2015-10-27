5f03abafc1e634a2678bd601ca3f33011bebdc44
Nicolas Gauche <gn005920@etu.unice.fr>
2015-10-27 14:14:21 +0100

    - Ajout d'une activité design selon l'exercice du cours (diapo 72, BuzzActivity) - Mise en configuration des dimensions des margin et padding de register (fichier dimens.xml)

---
+++ app/src/main/java/org/unice/mbds/tp1/tpandroid/BuzzActivity.java
@@ -0,0 +1,13 @@
+package org.unice.mbds.tp1.tpandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuzzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzz);
+    }
+}
