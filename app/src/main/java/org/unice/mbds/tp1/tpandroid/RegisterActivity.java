5a828a28d5b6c81ec29747e6bd7bdfffac26b25e
Nicolas Gauche <gn005920@etu.unice.fr>
2015-10-30 09:18:20 +0100

    register avec web service

--- app/src/main/java/org/unice/mbds/tp1/tpandroid/RegisterActivity.java
+++ app/src/main/java/org/unice/mbds/tp1/tpandroid/RegisterActivity.java
@@ -1,13 +1,27 @@
 package org.unice.mbds.tp1.tpandroid;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -86,7 +100,7 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         // Check for a valid phone
-        if (TextUtils.isEmpty(phone)|| !TextUtils.isDigitsOnly(phone) || !isPhoneValid(phone)) {
+        if (TextUtils.isEmpty(phone) || !TextUtils.isDigitsOnly(phone) || !isPhoneValid(phone)) {
             editPhone.setError(getString(R.string.error_invalid_phone));
             focusView = editPhone;
             cancel = true;
@@ -111,7 +125,109 @@ public class RegisterActivity extends AppCompatActivity {
             // form field with an error.
             focusView.requestFocus();
         } else {
-            Intent intent = new Intent(this, SigninActivity.class);
+            if(v.getId()==R.id.button_register) {
+
+                try {
+                    String sexe = "";
+                    if(Integer.toString(groupGenre.getCheckedRadioButtonId()).equals("2131492978")) {
+                        sexe = "M";
+                    } else {
+                        sexe = "F";
+                    }
+                    String[] params = new String[]{prenom, nom, sexe, phone, email, password};
+                    new MyTask().execute(params);
+                } catch (Exception e) {
+                    Log.w("Erreur register", e.getMessage());
+                }
+
+            }
+        }
+    }
+
+    class MyTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        public void showProgressDialog(boolean isVisible) {
+            if (isVisible) {
+                if(progressDialog==null) {
+                    progressDialog = new ProgressDialog(RegisterActivity.this);
+                    progressDialog.setMessage("Patientez...");
+                    progressDialog.setCancelable(false);
+                    progressDialog.setIndeterminate(true);
+                    progressDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            progressDialog = null;
+                        }
+                    });
+                    progressDialog.show();
+                }
+            }
+            else {
+                if(progressDialog!=null) {
+                    progressDialog.dismiss();
+                }
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                HttpClient client = new DefaultHttpClient();
+                HttpPost post = new HttpPost("http://92.243.14.22/person/");
+
+                // add header
+                post.setHeader("Content-Type", "application/json");
+                JSONObject obj = new JSONObject();
+                obj.put("nom", params[1]);
+                obj.put("prenom", params[0]);
+                obj.put("sexe", params[2]);
+                obj.put("telephone", params[3]);
+                obj.put("email", params[4]);
+                obj.put("createdby", params[0]);
+                obj.put("password", params[5]);
+
+                StringEntity entity = new StringEntity(obj.toString());
+                post.setEntity(entity);
+
+                HttpResponse response = client.execute(post);
+                Log.w("Sending :", "\nSending 'POST' request to URL : http://92.243.14.22/person/");
+                Log.w("Params :", "Post parameters : " + post.getEntity());
+                Log.w("Response :", "Response Code : " +
+                        response.getStatusLine().getStatusCode());
+
+                BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer result = new StringBuffer();
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+
+                System.out.println(result.toString());
+                return result.toString();
+            } catch (Exception e) {
+                Log.w("Erreur création : ", e.getMessage());
+                return "An error occured....";
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showProgressDialog(true);
+        }
+
+        @Override
+        protected void onPostExecute(String theResponse) {
+            super.onPostExecute(theResponse);
+            showProgressDialog(false);
+            Toast.makeText(RegisterActivity.this, "Inscription ok", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(RegisterActivity.this, SigninActivity.class);
             startActivity(intent);
         }
     }
